489f3c83d99437d00d0df96acfe443b0b49829f7
10ydaydream <ljqxnn@163.com>
2018-05-19 23:31:09 +0800

    test ssh push

--- app/src/main/java/com/daydream/lazyloadfragment/fragment/LazyLoadFragment.java
+++ app/src/main/java/com/daydream/lazyloadfragment/fragment/LazyLoadFragment.java
@@ -7,6 +7,8 @@ public class LazyLoadFragment extends Fragment {
     /*
     * 实现延迟加载的关键在于setUserVisibleHint方法
     * 在Fragment创建和显示时会调用setUserVisibleHint方法， 并且传入参数为true
+    *
+    * 还需要了解什么场景下setUserVisibleHint的调用和传入参数值
     * */
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
